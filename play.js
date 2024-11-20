10c73cde69c9c53b52776b880a728a013018fd8a
TayrineSoares <tayrinecristina@hotmail.com>
2024-11-20 11:55:36 -0500

    Implement connect function to provide a message when a new connection is made

--- play.js
+++ play.js
@@ -3,8 +3,13 @@ const net = require("net");
 // establishes a connection with the game server 
 const connect = function () {
   const conn = net.createConnection({
-    host: localhost, 
-    port: 5000
+    host: "localhost", 
+    port: 50541
+  });
+
+  // Providing a message when a successful connection is made. 
+  conn.on("connect", (data) => {
+    console.log("You have been connected!");
   });
 
 // interpret incoming data as text 
